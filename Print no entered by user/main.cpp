7ea8f5818f4742c1b2342a24cc6c14fc11bf3fbb
Samriddhi-dubey10 <74581936+Samriddhi-dubey10@users.noreply.github.com>
2020-11-17 10:00:32 +0530

    Add files via upload

---
+++ Print no entered by user/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a;
+    cout << "enter no" << endl;
+    cin>>a;
+    cout<<"displaying no given by user "<<a<<"\n";
+    return 0;
+}
